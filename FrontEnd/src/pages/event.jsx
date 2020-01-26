016ce64d7844fac70dd99e8f6a97bd8be61ba3dc
RdtBui <richard.bui@member.mensa.org>
2020-01-26 08:22:59 -0500

    UI frontend revamp

--- FrontEnd/src/pages/event.jsx
+++ FrontEnd/src/pages/event.jsx
@@ -1,55 +1,63 @@
-import React from 'react';
-import '../App.css';
-import axios from 'axios';
+import React from "react";
+import "../App.css";
+import axios from "axios";
 
 export default class EventPage extends React.Component {
-  
   state = {
-    type: 'default',
-    image: ''
+    type: "default",
+    image: ""
   };
 
   //get event type from BE
 
   componentDidMount() {
-    var type = this.state.type
+    var type = this.state.type;
 
-    var url = 'https://api.shutterstock.com/v2/images/search?query='+ type
-    console.log(url)
-    axios.get('https://api.shutterstock.com/v2/images/search?query=this.state.type', {
-      auth: {
-        username: '0BQG2MOeAYW71gyAa9eqmSqyoqpKSG5L',
-        password: 'R5pkYG1gX6hSGBTi'
-      }
-    }).then(({ data }) => {
-      console.log(data)
-      console.log(data.data.length)
-      var rndm = Math.floor(Math.random() * data.data.length) + 1 
-      console.log(rndm)
-      this.setState({
-        ...this.state,
-        image: data.data[rndm].assets.preview.url
-      })
-    })
+    var url = "https://api.shutterstock.com/v2/images/search?query=" + type;
+    console.log(url);
+    axios
+      .get(
+        "https://api.shutterstock.com/v2/images/search?query=this.state.type",
+        {
+          auth: {
+            username: "0BQG2MOeAYW71gyAa9eqmSqyoqpKSG5L",
+            password: "R5pkYG1gX6hSGBTi"
+          }
+        }
+      )
+      .then(({ data }) => {
+        console.log(data);
+        console.log(data.data.length);
+        var rndm = Math.floor(Math.random() * data.data.length) + 1;
+        console.log(rndm);
+        this.setState({
+          ...this.state,
+          image: data.data[rndm].assets.preview.url
+        });
+      });
   }
 
-  renderVolunteers(){
-    axios.get('').then(() => {
-      
-    });
+  renderVolunteers() {
+    axios.get("").then(() => {});
   }
 
   render() {
-  return (
-    <div className="App">
-      {this.state.image && <img src={this.state.image} />}
-      <h3> GET EVENT NAME  </h3>
-      <small>
-        Volunteers:
-      </small>
-      {/* loop volunteers and get name and timeslots */}
-      {/* GET FROM DB */}
-    </div>
-  )
-}
+    return (
+      <div className="App">
+        <img
+          src="communihub_logo.png"
+          alt="logo"
+          style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0 }}
+        />
+        {this.state.image && (
+          <img src={this.state.image} style={{ borderRadius: 400 / 2 }} />
+        )}
+        {this.state.image && <img src={this.state.image} />}
+        <h3> GET EVENT NAME </h3>
+        <small>Volunteers:</small>
+        {/* loop volunteers and get name and timeslots */}
+        {/* GET FROM DB */}
+      </div>
+    );
+  }
 }
--- FrontEnd/src/pages/index.jsx
+++ FrontEnd/src/pages/index.jsx
@@ -1,73 +1,82 @@
-
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-import Button from '@material-ui/core/Button';
-
-
+import axios from "axios";
+import Button from "@material-ui/core/Button";
 
 export default class MainPage extends React.Component {
+  state = {
+    projects: [],
+    image: ""
+  };
 
-
-    state = {
-      projects: [],
-      image: ''
-    };
-
-    componentDidMount() {
-      axios.get('https://api.shutterstock.com/v2/images/search?query=welcome', {
+  componentDidMount() {
+    axios
+      .get("https://api.shutterstock.com/v2/images/search?query=welcome", {
         auth: {
-          username: '0BQG2MOeAYW71gyAa9eqmSqyoqpKSG5L',
-          password: 'R5pkYG1gX6hSGBTi'
+          username: "0BQG2MOeAYW71gyAa9eqmSqyoqpKSG5L",
+          password: "R5pkYG1gX6hSGBTi"
         }
-      }).then(({ data }) => {
-        console.log(data)
-        console.log(data.data.length)
-        var rndm = Math.floor(Math.random() * data.data.length) + 1 
-        console.log(rndm)
+      })
+      .then(({ data }) => {
+        console.log(data);
+        console.log(data.data.length);
+        var rndm = Math.floor(Math.random() * data.data.length) + 1;
+        console.log(rndm);
         this.setState({
           ...this.state,
           image: data.data[rndm].assets.preview.url
-        })
-      })
-    }
-
-    renderEvents(){
-    axios.get('').then(() => {
-      
-    });
+        });
+      });
   }
 
-    render() {
-  return (
-    
-    <div className="App">
-        {this.state.image && <img src={this.state.image} />}
-        {/* display shutterstock welcome img */}
-        <Link to="/newEvent" style={{ textDecoration: 'none' }}> <Button variant="contained" color="secondary">
-        CREATE NEW EVENT
-</Button> </Link>
-      
-      <Link to="/volunteer" style={{ textDecoration: 'none' }}> <Button variant="contained" color="secondary">
-      NEW VOLUNTEER APPLICATION
-      </Button> </Link>
+  renderEvents() {
+    axios.get("").then(() => {});
+  }
 
-      <small>
-        ASSIGNMENTS:
-      </small>
-      {/* //demo */}
-      <Link to="/event" style={{ textDecoration: 'none' }}> <Button variant="outlined" color="secondary">
-      (arraynumber).eventName
-      </Button> </Link>
-      {/* loop events (for each row in result.data) */}
-      {/* <Link to="/event" style={{ textDecoration: 'none' }}> <Button variant="contained" color="secondary">
+  render() {
+    return (
+      <div className="App">
+        <img
+          src="communihub_logo.png"
+          alt="logo"
+          style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0 }}
+        />
+        {this.state.image && (
+          <img src={this.state.image} style={{ borderRadius: 400 / 2 }} />
+        )}
+        {/* display shutterstock welcome img */}
+        <Link to="/newEvent" style={{ textDecoration: "none" }}>
+          {" "}
+          <Button variant="contained" color="secondary">
+            CREATE NEW EVENT
+          </Button>{" "}
+        </Link>
+
+        <Link to="/volunteer" style={{ textDecoration: "none" }}>
+          {" "}
+          <Button variant="contained" color="secondary">
+            NEW VOLUNTEER APPLICATION
+          </Button>{" "}
+        </Link>
+
+        <small>ASSIGNMENTS:</small>
+        {/* //demo */}
+        <Link to="/event" style={{ textDecoration: "none" }}>
+          {" "}
+          <Button variant="outlined" color="secondary">
+            (arraynumber).eventName
+          </Button>{" "}
+        </Link>
+        {/* loop events (for each row in result.data) */}
+        {/* <Link to="/event" style={{ textDecoration: 'none' }}> <Button variant="contained" color="secondary">
       (arraynumber).eventName
       </Button> </Link> */}
-      {/* GET FROM DB */}
-      {this.state.projects.map(projects => <li>{projects.name}</li>)}
-    </div>
-  )
-}
-
+        {/* GET FROM DB */}
+        {this.state.projects.map(projects => (
+          <li>{projects.name}</li>
+        ))}
+      </div>
+    );
+  }
 }
