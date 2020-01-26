657be063f45019e1556a432810923484cd96e107
niiita <nitachaska@gmail.com>
2020-01-25 19:22:43 -0500

    setup continued

--- FrontEnd/frontend/src/App.js
+++ FrontEnd/frontend/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
 
 //Pages
 import MainPage from "./pages";
+import NewEventPage from './pages/newEvent';
+
+import VolunteerPage from './pages/volunteer';
 
 class App extends Component {
   render() {
     return <Router>
-      <Route path="/" component={MainPage} />
+      <Switch>
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/newEvent" component={NewEventPage} />
+      <Route exact path="/volunteer" component={VolunteerPage} />
+      </Switch>
     </Router>
   }
 }
--- FrontEnd/frontend/src/pages/newEvent.jsx
+++ FrontEnd/frontend/src/pages/newEvent.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import '../App.css';
 
 const NewEventPage = () => {
   return (
-    <div>
+    <div className="App">
+        <h3>CREATE A NEW EVENT</h3>
           {/* Make form to create new event */}
     </div>
   )
